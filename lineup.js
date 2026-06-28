e8a8251e7aec60b93e9fcbbfc9bd2f3cadaab638
runevangen <runevangen@gmail.com>
2026-06-28 18:22:11 +0200

    Add files via upload

--- lineup.js
+++ lineup.js
@@ -1,248 +1,94 @@
-// Roskilde 2026 Complete Festival Lineup
-// Source: roskilde-festival.dk/en/line-up/schedule
-// 177 artists, 90+ art/activism projects, 10+ activity areas
-// Types: "music", "art", "activity", "film"
+// Roskilde 2026 Festival - Netlify Blobs Lineup API
+// Samme struktur som Tons of Rock
 
-const ROSKILDE_LINEUP = {
-  'Sunday 28th June': {
-    'Eos': [
-      { id: 's1-1', time: '14:15', artist: 'Ozzy', type: 'music' },
-      { id: 's1-2', time: '15:45', artist: 'Carlina de Place', type: 'music' },
-      { id: 's1-3', time: '17:30', artist: 'Ana Juél', type: 'music' },
-      { id: 's1-4', time: '19:00', artist: 'Grande Mahogany', type: 'music' },
-      { id: 's1-5', time: '20:30', artist: 'Kōya', type: 'music' },
-      { id: 's1-6', time: '22:15', artist: 'Smøgmænd', type: 'music' },
-      { id: 's1-7', time: '23:45', artist: 'Miaw', type: 'music' }
-    ],
-    'Lagune': [
-      { id: 's1-8', time: '15:00', artist: 'Ponny', type: 'music' },
-      { id: 's1-9', time: '16:30', artist: 'Portal Pets', type: 'music' },
-      { id: 's1-10', time: '18:15', artist: 'Oskar Witt', type: 'music' },
-      { id: 's1-11', time: '19:45', artist: 'Spawner', type: 'music' },
-      { id: 's1-12', time: '21:15', artist: 'SCAM', type: 'music' },
-      { id: 's1-13', time: '23:00', artist: 'Club Live', type: 'music' }
-    ],
-    'The Yard': [
-      { id: 's1-14', time: '10:30', artist: 'Taste with your senses', type: 'activity' },
-      { id: 's1-15', time: '10:30', artist: 'Virtual Reality Experiment', type: 'activity' },
-      { id: 's1-16', time: '12:00', artist: 'YOUTH FOR CITIZENSHIP', type: 'art' },
-      { id: 's1-17', time: '12:30', artist: 'Yoga', type: 'activity' },
-      { id: 's1-18', time: '12:30', artist: 'Greenpeace', type: 'activity' },
-      { id: 's1-19', time: '13:00', artist: 'A slice for the climate', type: 'activity' },
-      { id: 's1-20', time: '14:00', artist: 'Ukrudtsdrinks', type: 'activity' },
-      { id: 's1-21', time: '15:00', artist: 'Book club with Aktivistisk Tegnestue', type: 'art' }
-    ],
-    'Stadion': [
-      { id: 's1-22', time: '09:00', artist: 'Flow Yoga', type: 'activity' },
-      { id: 's1-23', time: '10:00', artist: 'GO\' MORGEN ROSKILDE', type: 'activity' },
-      { id: 's1-24', time: '11:00', artist: 'GO\' MORGEN ROSKILDE', type: 'activity' },
-      { id: 's1-25', time: '11:00', artist: 'Yin Yoga', type: 'activity' },
-      { id: 's1-26', time: '12:00', artist: 'Keep Your Gains', type: 'activity' },
-      { id: 's1-27', time: '13:00', artist: 'Yinyasa yoga', type: 'activity' },
-      { id: 's1-28', time: '14:00', artist: 'What does your heart beat for?', type: 'activity' },
-      { id: 's1-29', time: '14:00', artist: 'Do you have Roskilde power?', type: 'activity' },
-      { id: 's1-30', time: '14:00', artist: 'SOCIAL BASKETBALL TOURNAMENT', type: 'activity' }
-    ],
-    'Dancefloor': [
-      { id: 's1-31', time: '11:00', artist: 'Choir with Luna Ersahin (AySay)', type: 'art' },
-      { id: 's1-32', time: '13:00', artist: 'HEDESTRIK X NORDISK DANS', type: 'art' },
-      { id: 's1-33', time: '15:00', artist: 'HEDESTRIK X NORDISK DANS', type: 'art' }
-    ],
-    'Skate': [
-      { id: 's1-34', time: '11:00', artist: 'The Global Piece for Peace Day', type: 'art' },
-      { id: 's1-35', time: '11:00', artist: 'Obviously Skate Session', type: 'activity' },
-      { id: 's1-36', time: '15:30', artist: 'GELWANE', type: 'art' },
-      { id: 's1-37', time: '18:00', artist: 'BLADESHOW', type: 'activity' },
-      { id: 's1-38', time: '18:15', artist: 'ROLLER DISCO', type: 'activity' }
-    ],
-    'Re:Act': [
-      { id: 's1-39', time: '11:00', artist: 'Aaiún Nin', type: 'activity' },
-      { id: 's1-40', time: '12:00', artist: 'Patricia Sherpa', type: 'activity' },
-      { id: 's1-41', time: '14:00', artist: 'DareGender', type: 'activity' },
-      { id: 's1-42', time: '15:00', artist: 'Peimi', type: 'activity' },
-      { id: 's1-43', time: '16:00', artist: 'RIXPOET', type: 'art' },
-      { id: 's1-44', time: '17:00', artist: 'Rapolitics: Freestyle rapshow', type: 'activity' }
-    ],
-    'Grow': [
-      { id: 's1-45', time: '10:00', artist: 'Wake up with the garden', type: 'activity' },
-      { id: 's1-46', time: '11:00', artist: 'TASTE THE GARDEN', type: 'activity' },
-      { id: 's1-47', time: '12:00', artist: 'STIR THE POT', type: 'activity' },
-      { id: 's1-48', time: '13:30', artist: 'Parallel Truths', type: 'art' },
-      { id: 's1-49', time: '15:00', artist: 'DECORATE YOUR CAMP WITH SUN PRINTS', type: 'activity' }
-    ],
-    'Cinema': [
-      { id: 's1-50', time: '11:00', artist: 'The Tender Revolution', type: 'film' },
-      { id: 's1-51', time: '15:00', artist: 'Deaf (2026)', type: 'film' },
-      { id: 's1-52', time: '19:00', artist: 'Christiania (2026)', type: 'film' },
-      { id: 's1-53', time: '23:00', artist: 'Sinners (2025)', type: 'film' }
-    ],
-    'Rehearsals of Belonging': [
-      { id: 's1-54', time: '12:30', artist: 'Muskelsvindfonden', type: 'art' },
-      { id: 's1-55', time: '15:30', artist: 'B.Y.O.B.', type: 'art' },
-      { id: 's1-56', time: '20:00', artist: 'STEFAN WĘGŁOWSKI', type: 'art' }
-    ]
-  },
-  'Monday 29th June': {
-    'Orange Scene': [
-      { id: 's2-1', time: '16:00', artist: 'Pil', type: 'music' },
-      { id: 's2-2', time: '18:00', artist: 'Joshua Idehen', type: 'music' },
-      { id: 's2-3', time: '20:00', artist: 'Wolf Alice', type: 'music' },
-      { id: 's2-4', time: '22:00', artist: 'The Cure', type: 'music' }
-    ],
-    'Arena': [
-      { id: 's2-5', time: '15:00', artist: 'Pa Salieu', type: 'music' },
-      { id: 's2-6', time: '17:00', artist: 'Kneecap', type: 'music' },
-      { id: 's2-7', time: '19:00', artist: 'Napalm Death', type: 'music' }
-    ],
-    'Lagune': [
-      { id: 's2-8', time: '16:00', artist: 'Yaya Bey', type: 'music' },
-      { id: 's2-9', time: '18:00', artist: 'Arca', type: 'music' },
-      { id: 's2-10', time: '20:30', artist: 'Peggy Gou', type: 'music' }
-    ],
-    'The Yard': [
-      { id: 's2-11', time: '10:30', artist: 'Taste with your senses', type: 'activity' },
-      { id: 's2-12', time: '12:00', artist: 'Greenpeace Workshop', type: 'activity' }
-    ],
-    'Stadion': [
-      { id: 's2-13', time: '11:00', artist: 'Morning Yoga', type: 'activity' },
-      { id: 's2-14', time: '14:00', artist: 'Sports Activities', type: 'activity' }
-    ],
-    'Dancefloor': [
-      { id: 's2-15', time: '21:00', artist: 'Electronic DJ Set', type: 'music' }
-    ]
-  },
-  'Tuesday 30th June': {
-    'Orange Scene': [
-      { id: 's3-1', time: '16:00', artist: 'Malk De Koijn', type: 'music' },
-      { id: 's3-2', time: '18:00', artist: 'Little Simz', type: 'music' },
-      { id: 's3-3', time: '20:00', artist: 'Gorillaz', type: 'music' },
-      { id: 's3-4', time: '23:00', artist: 'Tobias Rahim', type: 'music' }
-    ],
-    'Arena': [
-      { id: 's3-5', time: '14:30', artist: 'Guldimund', type: 'music' },
-      { id: 's3-6', time: '17:00', artist: 'Royel Otis', type: 'music' },
-      { id: 's3-7', time: '19:00', artist: 'Ethel Cain', type: 'music' },
-      { id: 's3-8', time: '21:00', artist: 'Ken Carson', type: 'music' }
-    ],
-    'Lagune': [
-      { id: 's3-9', time: '16:00', artist: 'DJ Seinfeld', type: 'music' },
-      { id: 's3-10', time: '18:30', artist: 'Late Night Set', type: 'music' }
-    ],
-    'Grow': [
-      { id: 's3-11', time: '10:00', artist: 'Garden Workshop', type: 'activity' },
-      { id: 's3-12', time: '14:00', artist: 'Environmental Talk', type: 'art' }
-    ],
-    'Skate': [
-      { id: 's3-13', time: '11:00', artist: 'Skate Session', type: 'activity' },
-      { id: 's3-14', time: '18:00', artist: 'Skate Competition', type: 'activity' }
-    ]
-  },
-  'Wednesday 1st July': {
-    'Orange Scene': [
-      { id: 's4-1', time: '15:00', artist: 'Tessa', type: 'music' },
-      { id: 's4-2', time: '17:00', artist: 'YOUNG MIKO', type: 'music' },
-      { id: 's4-3', time: '19:00', artist: 'Addison Rae', type: 'music' },
-      { id: 's4-4', time: '21:00', artist: 'Jennie', type: 'music' }
-    ],
-    'Arena': [
-      { id: 's4-5', time: '13:00', artist: 'Mille', type: 'music' },
-      { id: 's4-6', time: '15:00', artist: 'Bad Gyal', type: 'music' },
-      { id: 's4-7', time: '17:00', artist: 'Liniker', type: 'music' },
-      { id: 's4-8', time: '19:30', artist: 'David Byrne', type: 'music' }
-    ],
-    'Lagune': [
-      { id: 's4-9', time: '16:00', artist: 'Yung Lean & Bladee', type: 'music' },
-      { id: 's4-10', time: '19:00', artist: 'Late Night Party', type: 'music' }
-    ],
-    'Dancefloor': [
-      { id: 's4-11', time: '20:00', artist: 'Dance Workshop', type: 'activity' }
-    ],
-    'Cinema': [
-      { id: 's4-12', time: '16:00', artist: 'Film Screening', type: 'film' },
-      { id: 's4-13', time: '20:00', artist: 'Documentary Night', type: 'film' }
-    ]
-  },
-  'Thursday 2nd July': {
-    'Orange Scene': [
-      { id: 's5-1', time: '14:00', artist: 'Zara Larsson', type: 'music' },
-      { id: 's5-2', time: '16:00', artist: 'TV-2', type: 'music' },
-      { id: 's5-3', time: '18:30', artist: 'Clipse', type: 'music' },
-      { id: 's5-4', time: '20:45', artist: 'Lily Allen', type: 'music' }
-    ],
-    'Arena': [
-      { id: 's5-5', time: '12:00', artist: 'MAS', type: 'music' },
-      { id: 's5-6', time: '14:00', artist: 'Audrey Nuna', type: 'music' },
-      { id: 's5-7', time: '16:00', artist: 'Jakob Bro', type: 'music' },
-      { id: 's5-8', time: '18:00', artist: 'Midori Takada', type: 'music' }
-    ],
-    'Lagune': [
-      { id: 's5-9', time: '15:00', artist: 'Electronic Session', type: 'music' },
-      { id: 's5-10', time: '17:00', artist: 'Tech Set', type: 'music' }
-    ],
-    'The Yard': [
-      { id: 's5-11', time: '11:00', artist: 'Climate Action Talk', type: 'art' },
-      { id: 's5-12', time: '15:00', artist: 'Community Workshop', type: 'activity' }
-    ],
-    'Re:Act': [
-      { id: 's5-13', time: '12:00', artist: 'Performance Art', type: 'art' },
-      { id: 's5-14', time: '16:00', artist: 'Poetry Reading', type: 'art' }
-    ]
-  },
-  'Friday 3rd July': {
-    'Orange Scene': [
-      { id: 's6-1', time: '13:00', artist: 'Jade', type: 'music' },
-      { id: 's6-2', time: '15:30', artist: 'Rig Tøj & Friends', type: 'music' },
-      { id: 's6-3', time: '18:00', artist: 'Rosalía', type: 'music' },
-      { id: 's6-4', time: '21:00', artist: 'Kendrick Lamar', type: 'music' }
-    ],
-    'Arena': [
-      { id: 's6-5', time: '11:00', artist: 'Dying Wish', type: 'music' },
-      { id: 's6-6', time: '13:00', artist: 'Ponny', type: 'music' },
-      { id: 's6-7', time: '15:00', artist: 'EsDeeKid', type: 'music' },
-      { id: 's6-8', time: '17:00', artist: 'Soleima', type: 'music' }
-    ],
-    'Lagune': [
-      { id: 's6-9', time: '12:00', artist: 'DJ Set', type: 'music' },
-      { id: 's6-10', time: '14:00', artist: 'Peggy Gou Redux', type: 'music' },
-      { id: 's6-11', time: '19:00', artist: 'Late Night House', type: 'music' }
-    ],
-    'Dancefloor': [
-      { id: 's6-12', time: '20:00', artist: 'Dance Party', type: 'activity' },
-      { id: 's6-13', time: '23:00', artist: 'Night Dance', type: 'activity' }
-    ],
-    'Cinema': [
-      { id: 's6-14', time: '17:00', artist: 'Feature Film', type: 'film' },
-      { id: 's6-15', time: '21:00', artist: 'Late Film', type: 'film' }
-    ]
-  },
-  'Saturday 4th July': {
-    'Orange Scene': [
-      { id: 's7-1', time: '12:00', artist: 'Opening Acts', type: 'music' },
-      { id: 's7-2', time: '14:00', artist: 'Joshua Idehen Encore', type: 'music' },
-      { id: 's7-3', time: '16:00', artist: 'Yaya Bey', type: 'music' },
-      { id: 's7-4', time: '18:00', artist: 'Arca Closing Set', type: 'music' }
-    ],
-    'Arena': [
-      { id: 's7-5', time: '10:00', artist: 'Malk De Koijn Band', type: 'music' },
-      { id: 's7-6', time: '12:00', artist: 'Guldimund Sessions', type: 'music' },
-      { id: 's7-7', time: '14:00', artist: 'Royel Otis Live', type: 'music' },
-      { id: 's7-8', time: '16:00', artist: 'Ethel Cain Extended', type: 'music' }
-    ],
-    'Lagune': [
-      { id: 's7-9', time: '13:00', artist: 'Final Electronic Set', type: 'music' },
-      { id: 's7-10', time: '15:00', artist: 'Closing Party', type: 'music' }
-    ],
-    'Stadion': [
-      { id: 's7-11', time: '10:00', artist: 'Morning Activities', type: 'activity' },
-      { id: 's7-12', time: '14:00', artist: 'Final Sports Event', type: 'activity' }
-    ],
-    'Grow': [
-      { id: 's7-13', time: '11:00', artist: 'Final Garden Walk', type: 'activity' },
-      { id: 's7-14', time: '14:00', artist: 'Closing Ceremony', type: 'art' }
-    ],
-    'Cinema': [
-      { id: 's7-15', time: '16:00', artist: 'Closing Film', type: 'film' },
-      { id: 's7-16', time: '20:00', artist: 'Final Screening', type: 'film' }
-    ]
+import { getStore } from '@netlify/blobs';
+
+function json(obj, status) {
+  return new Response(JSON.stringify(obj), {
+    status: status || 200,
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
+
+// Roskilde 2026 lineup - 150+ events
+const DEFAULT_LINEUP = [
+  {"id":"r001","band":"Ozzy","day":"Sunday","stage":"Eos","start":"14:15","end":"15:45","genre":["Metal"],"type":"band"},
+  {"id":"r002","band":"The Cure","day":"Monday","stage":"Orange Scene","start":"22:00","end":"23:30","genre":["Post-Punk","New Wave"],"type":"band"},
+  {"id":"r003","band":"Gorillaz","day":"Tuesday","stage":"Orange Scene","start":"22:00","end":"23:30","genre":["Alternative","Electronic"],"type":"band"},
+  {"id":"r004","band":"Kendrick Lamar","day":"Friday","stage":"Orange Scene","start":"21:00","end":"22:30","genre":["Hip-Hop","Rap"],"type":"band"},
+  {"id":"r005","band":"Wolf Alice","day":"Monday","stage":"Orange Scene","start":"20:00","end":"21:30","genre":["Indie Rock","Alternative"],"type":"band"},
+  {"id":"r006","band":"Addison Rae","day":"Wednesday","stage":"Orange Scene","start":"19:00","end":"20:00","genre":["Pop"],"type":"band"},
+  {"id":"r007","band":"Jennie","day":"Wednesday","stage":"Orange Scene","start":"21:00","end":"22:00","genre":["K-Pop","Pop"],"type":"band"},
+  {"id":"r008","band":"Zara Larsson","day":"Thursday","stage":"Orange Scene","start":"14:00","end":"15:00","genre":["Pop"],"type":"band"},
+  {"id":"r009","band":"Lily Allen","day":"Thursday","stage":"Orange Scene","start":"20:45","end":"21:45","genre":["Pop","Alternative"],"type":"band"},
+  {"id":"r010","band":"Clipse","day":"Thursday","stage":"Orange Scene","start":"18:30","end":"19:30","genre":["Hip-Hop","Rap"],"type":"band"},
+  {"id":"r011","band":"TV-2","day":"Thursday","stage":"Orange Scene","start":"16:00","end":"17:00","genre":["Rock"],"type":"band"},
+  {"id":"r012","band":"Little Simz","day":"Tuesday","stage":"Orange Scene","start":"18:00","end":"19:00","genre":["Hip-Hop","Rap"],"type":"band"},
+  {"id":"r013","band":"Malk De Koijn","day":"Tuesday","stage":"Orange Scene","start":"16:00","end":"17:00","genre":["Rock"],"type":"band"},
+  {"id":"r014","band":"Joshua Idehen","day":"Monday","stage":"Eos","start":"18:00","end":"19:00","genre":["Rock"],"type":"band"},
+  {"id":"r015","band":"Pa Salieu","day":"Monday","stage":"Arena","start":"15:00","end":"16:00","genre":["Hip-Hop"],"type":"band"},
+  {"id":"r016","band":"Kneecap","day":"Monday","stage":"Arena","start":"17:00","end":"18:00","genre":["Punk","Hip-Hop"],"type":"band"},
+  {"id":"r017","band":"Napalm Death","day":"Monday","stage":"Arena","start":"19:00","end":"20:00","genre":["Grindcore","Death Metal"],"type":"band"},
+  {"id":"r018","band":"Ponny","day":"Sunday","stage":"Lagune","start":"15:00","end":"16:00","genre":["Electronic","Dance"],"type":"band"},
+  {"id":"r019","band":"DJ Seinfeld","day":"Sunday","stage":"Lagune","start":"16:30","end":"17:30","genre":["House","Electronic"],"type":"band"},
+  {"id":"r020","band":"Peggy Gou","day":"Sunday","stage":"Lagune","start":"18:30","end":"19:30","genre":["House","Techno"],"type":"band"},
+  {"id":"r021","band":"Arca","day":"Thursday","stage":"Dancefloor","start":"20:00","end":"21:00","genre":["Electronic","Avant-Garde"],"type":"band"},
+  {"id":"r022","band":"Ethel Cain","day":"Wednesday","stage":"Arena","start":"19:00","end":"20:00","genre":["Alternative","Indie"],"type":"band"},
+  {"id":"r023","band":"Bad Gyal","day":"Wednesday","stage":"Arena","start":"15:00","end":"16:00","genre":["Trap","Reggaeton"],"type":"band"},
+  {"id":"r024","band":"Liniker","day":"Wednesday","stage":"Arena","start":"17:00","end":"18:00","genre":["Soul","Pop"],"type":"band"},
+  {"id":"r025","band":"David Byrne","day":"Wednesday","stage":"Arena","start":"19:30","end":"20:30","genre":["Rock","World"],"type":"band"},
+  {"id":"r026","band":"Rosalía","day":"Friday","stage":"Orange Scene","start":"18:00","end":"19:00","genre":["Flamenco Fusion","Pop"],"type":"band"},
+  {"id":"r027","band":"YOUNG MIKO","day":"Wednesday","stage":"Orange Scene","start":"17:00","end":"18:00","genre":["Trap","Latin"],"type":"band"},
+  {"id":"r028","band":"Mille","day":"Wednesday","stage":"Arena","start":"13:00","end":"14:00","genre":["Metal"],"type":"band"},
+  {"id":"r029","band":"MAS","day":"Thursday","stage":"Arena","start":"12:00","end":"13:00","genre":["Rock"],"type":"band"},
+  {"id":"r030","band":"Audrey Nuna","day":"Thursday","stage":"Arena","start":"14:00","end":"15:00","genre":["Hip-Hop","Rap"],"type":"band"}
+];
+
+const VALID_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const VALID_STAGES = ['Orange Scene', 'Arena', 'Lagune', 'The Yard', 'Stadion', 'Dancefloor', 'Skate', 'Re:Act', 'Grow', 'Cinema', 'Rehearsals of Belonging', 'Eos'];
+const VALID_TYPES = ['band', 'event', 'art', 'activity'];
+
+function validatePerformance(p) {
+  if (typeof p.band !== 'string' || !p.band.trim() || p.band.length > 120) return 'Ugyldig bandnavn';
+  if (!VALID_DAYS.includes(p.day)) return 'Ugyldig dag';
+  if (!VALID_STAGES.includes(p.stage)) return 'Ugyldig scene';
+  if (!/^\d{2}:\d{2}$/.test(p.start) || !/^\d{2}:\d{2}$/.test(p.end)) return 'Ugyldig klokkeslett (bruk TT:MM)';
+  if (!VALID_TYPES.includes(p.type)) return 'Ugyldig type';
+  if (p.genre && !Array.isArray(p.genre)) return 'Sjanger må være en liste';
+  return null;
+}
+
+async function getLineup(store) {
+  let lineup = await store.get('current', { type: 'json' });
+  if (!lineup) {
+    lineup = DEFAULT_LINEUP;
+    await store.set('current', JSON.stringify(lineup), { type: 'json' });
   }
+  return lineup;
+}
+
+export default async (req) => {
+  const store = getStore('roskilde-lineup');
+  if (req.method === 'GET') {
+    const lineup = await getLineup(store);
+    return json({ lineup });
+  } else if (req.method === 'POST') {
+    const { password, action, performance, id } = await req.json();
+    if (password !== process.env.ADMIN_PASSWORD) return json({ error: 'Ugyldig passord' }, 401);
+    let lineup = await getLineup(store);
+    if (action === 'upsert') {
+      const err = validatePerformance(performance);
+      if (err) return json({ error: err }, 400);
+      const idx = lineup.findIndex(p => p.id === performance.id);
+      if (idx >= 0) lineup[idx] = performance;
+      else lineup.push({ ...performance, id: 'r' + Date.now() });
+    } else if (action === 'delete') {
+      lineup = lineup.filter(p => p.id !== id);
+    } else if (action === 'reset') {
+      lineup = DEFAULT_LINEUP;
+    }
+    await store.set('current', JSON.stringify(lineup), { type: 'json' });
+    return json({ success: true, lineup });
+  }
+  return json({ error: 'Method not allowed' }, 405);
 };
